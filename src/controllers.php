2319c805736ff0f448d778e413b4d7f189010f0d
Takashi Kanemoto <t-kanemoto@quartet-mail.com>
2014-08-29 13:58:19 +0900

    rename template files from *.html to *.html.twig

--- src/controllers.php
+++ src/controllers.php
@@ -9,7 +9,7 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 //Request::setTrustedProxies(array('127.0.0.1'));
 
 $app->get('/', function () use ($app) {
-    return $app['twig']->render('index.html', array());
+    return $app['twig']->render('index.html.twig', array());
 })
 ->bind('homepage')
 ;
@@ -19,12 +19,12 @@ $app->error(function (\Exception $e, $code) use ($app) {
         return;
     }
 
-    // 404.html, or 40x.html, or 4xx.html, or error.html
+    // 404.html.twig, or 40x.html.twig, or 4xx.html.twig, or error.html.twig
     $templates = array(
-        'errors/'.$code.'.html',
-        'errors/'.substr($code, 0, 2).'x.html',
-        'errors/'.substr($code, 0, 1).'xx.html',
-        'errors/default.html',
+        'errors/'.$code.'.html.twig',
+        'errors/'.substr($code, 0, 2).'x.html.twig',
+        'errors/'.substr($code, 0, 1).'xx.html.twig',
+        'errors/default.html.twig',
     );
 
     return new Response($app['twig']->resolveTemplate($templates)->render(array('code' => $code)), $code);
