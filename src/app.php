6187338903cf4392def065b7152afde55aa1257b
Takashi Kanemoto <t-kanemoto@quartet-mail.com>
2014-08-29 14:36:04 +0900

    install twig text extension

--- src/app.php
+++ src/app.php
@@ -13,7 +13,7 @@ $app->register(new ServiceControllerServiceProvider());
 $app->register(new TwigServiceProvider());
 $app['twig'] = $app->share($app->extend('twig', function ($twig, $app) {
     // add custom globals, filters, tags, ...
-
+    $twig->addExtension(new \Twig_Extensions_Extension_Text($app));
     return $twig;
 }));
 
